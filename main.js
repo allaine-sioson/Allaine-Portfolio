83daf6b8d7ed8407cf1ebc704e08d0d096b7ecd5
allaine-sioson <allaine.sioson@gmail.com>
2025-02-23 17:07:20 -0500

    fix: scrolling issue

--- main.js
+++ main.js
@@ -31,66 +31,102 @@ document.body.onpointermove = event => {
 		fill: 'forwards'
 	});
 }
-
-
-function displayProfile() {
-    text1.style.fontFamily = "headerFont";
-    text1.style.fontSize = "200%"
-    text1.style.color = "var(--header-color)";
-    text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5)";
-    text1.style.marginTop = "10%";
-
-    text1.innerHTML = "Hello! Welcome to my Portfolio.<br><br>My name is Allaine Sioson and I am an aspiring UI/UX Designer and App developer.<br><br>I currently have a couple projects that I am working on which you may find in the Projects section of this website.<br><br>Languages I use:";
-
-    text2.innerHTML = "";
-
-    java.src= "images/java.png";
-    python.src= "images/python.png";
-    arduino.src= "images/arduino.png";
-    html.src= "images/html.png";
-    css.src= "images/css.png";
-    js.src= "images/js.png";
+// Function to hide the .right div (fade out and scale down)
+function hideInfo() {
+    // Adding a small delay to allow the fade-out effect before the content is replaced
+    right.classList.remove('active');
+    setTimeout(() => {
+        right.style.opacity = '0';  // Set opacity to 0 for fade-out effect
+    }, 200);  // 200ms delay before starting fade-out
 }
 
-function displayexperiences() {
-    text1.style.fontFamily = "hyperlinkFont";
-    text1.style.fontSize = "150%"
-    text1.style.color = "var(--subheader-color)";
-    text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5), 2px 2px 5px rgb(40, 32, 2)";
-    text1.style.marginTop = "7.5%";
+// Function to activate the .right div with animation (fade in)
+function showInfo() {
+    right.style.opacity = '1';  // Ensure opacity is 1 when showing content
+    right.classList.add('active');
+}
 
-    text1.innerHTML = "IOS App Developer<br>Career Education Council (Co-op)<br>Jul 2024 - Jul 2024";
+function displayProfile() {
+    hideInfo(); // Hide previous content
 
-    text2.innerHTML = "<br><br>I worked in the \"Develop The Future\" program where I was tasked with completing various Swift playgrounds, creating wireframes and prototypes, researching different app concepts and creating an app pitch presentation at the end of the program. During my time at this placement I have gained the skills to use the Swift programming language while simultaneously learning the XCode program.<br><br> Responsibilities :<br><br> - Attend daily meetings<br> - Complete weekly tasks assigned (e.g. Playgrounds, Apps, Planners)<br> - Participate in creative workshops<br> - Create wireframes<br> - Develop Apps";
+    // Wait for fade-out to finish before showing new content
+    setTimeout(() => {
+        showInfo(); // Show the new section (fade-in)
+        text1.style.fontFamily = "headerFont";
+        text1.style.fontSize = "200%";
+        text1.style.color = "var(--header-color)";
+        text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5)";
+        text1.innerHTML = "Hello! Welcome to my Portfolio.<br><br>My name is Allaine Sioson and I am an aspiring UI/UX Designer and App developer.<br><br>I currently have a couple projects that I am working on which you may find in the Projects section of this website.<br><br>Languages I use:";
 
-    java.src= "";
-    python.src= "";
-    arduino.src= "";
-    html.src= "";
-    css.src= "";
-    js.src= "";
-    
+        text2.innerHTML = "";
+
+        // Update image sources
+        java.src = "images/java.png";
+        python.src = "images/python.png";
+        arduino.src = "images/arduino.png";
+        html.src = "images/html.png";
+        css.src = "images/css.png";
+        js.src = "images/js.png";
+    }, 500); // 500ms delay before the new content is displayed after fade-out
+}
+
+function displayExperiences() {
+    hideInfo(); // Hide previous content
+
+    // Wait for fade-out to finish before showing new content
+    setTimeout(() => {
+        showInfo(); // Show the new section (fade-in)
+        text1.style.fontFamily = "hyperlinkFont";
+        text1.style.fontSize = "150%";
+        text1.style.color = "var(--subheader-color)";
+        text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5), 2px 2px 5px rgb(40, 32, 2)";
+        text1.innerHTML = "IOS App Developer<br>Career Education Council (Co-op)<br>Jul 2024 - Jul 2024";
+
+        text2.innerHTML = "<br><br>I worked in the 'Develop The Future' program where I was tasked with completing various Swift playgrounds, creating wireframes and prototypes, researching different app concepts and creating an app pitch presentation at the end of the program. During my time at this placement, I have gained the skills to use the Swift programming language while simultaneously learning the XCode program.<br><br> Responsibilities :<br><br> - Attend daily meetings<br> - Complete weekly tasks assigned (e.g., Playgrounds, Apps, Planners)<br> - Participate in creative workshops<br> - Create wireframes<br> - Develop Apps";
+
+        // Remove image sources for experiences
+        java.src = "";
+        python.src = "";
+        arduino.src = "";
+        html.src = "";
+        css.src = "";
+        js.src = "";
+    }, 500); // 500ms delay before the new content is displayed after fade-out
 }
 
 function displayProjects() {
-    text1.style.fontFamily = "headerFont";
-    text1.style.fontSize = "200%"
-    text1.style.color = "var(--header-color)";
-    text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5)";
-    text1.style.marginTop = "10%";
+    hideInfo(); // Hide previous content
 
-    text1.innerHTML = "Placeholder Text";
-    text2.innerHTML = "";
+    // Wait for fade-out to finish before showing new content
+    setTimeout(() => {
+        showInfo(); // Show the new section (fade-in)
+        text1.style.fontFamily = "headerFont";
+        text1.style.fontSize = "200%";
+        text1.style.color = "var(--header-color)";
+        text1.style.textShadow = "2px 2px 5px rgb(192, 148, 5)";
+        text1.innerHTML = "Placeholder Text";
 
-    java.src= "";
-    python.src= "";
-    arduino.src= "";
-    html.src= "";
-    css.src= "";
-    js.src= "";
+        text2.innerHTML = "";
+
+        // Remove image sources for projects
+        java.src = "";
+        python.src = "";
+        arduino.src = "";
+        html.src = "";
+        css.src = "";
+        js.src = "";
+    }, 500); // 500ms delay before the new content is displayed after fade-out
 }
 
-profile.addEventListener("mouseover", displayProfile);
-experiences.addEventListener("mouseover", displayexperiences);
-projects.addEventListener("mouseover", displayProjects);
-// credits.addEventListener("mouseover", displayCredits);
+// Add event listeners to buttons
+profile.addEventListener('click', () => {
+    displayProfile();
+});
+
+experiences.addEventListener('click', () => {
+    displayExperiences();
+});
+
+projects.addEventListener('click', () => {
+    displayProjects();
+});
